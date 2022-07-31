80f10a10155fd8c8a4dd63c6613d36e249d86b6a
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-07-30 22:32:54 -0600

    Edit the on Delete behavior to avoid circular deletions with FKs.

--- FlightsManager/DB/ApplicationDBContext.cs
+++ FlightsManager/DB/ApplicationDBContext.cs
@@ -14,15 +14,14 @@ namespace FlightsManager.DB
 
         public DbSet<User> Users { get; set; }
         public DbSet<Aerolinea> Aerolineas { get; set; }
-        public DbSet<Avion> Aviones { get; set; }
+        public DbSet<Aeropuerto> Aeropuertos { get; set; }
         public DbSet<Asiento> Asientos { get; set; }
+        public DbSet<Avion> Aviones { get; set; }
+        public DbSet<Horario> Horarios { get; set; }
+        public DbSet<Pais> Paises { get; set; }
+        public DbSet<Reservas> Reservas { get; set; }
         public DbSet<Tarifa> Tarifas { get; set; }
         public DbSet<Vuelo> Vuelos { get; set; }
-        public DbSet<Reserva> Reservas { get; set; }
-        public DbSet<Pago> Pagos { get; set; }
-        public DbSet<Pais> Paises { get; set; }
-        public DbSet<Aeropuerto> Aeropuertos { get; set; }
-        public DbSet<Horario> Horarios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -31,6 +30,17 @@ namespace FlightsManager.DB
             builder.Entity<User>()
                 .HasIndex(u => u.UserId)
                 .IsUnique();
+
+            builder.Entity<Vuelo>()
+                .HasOne(v => v.AeropuertoDestino)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<Vuelo>()
+                .HasOne(v => v.AeropuertoPartida)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+
         }
 
     }
